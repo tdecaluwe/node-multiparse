781a6224a2b6e09ae49a8dd034662062143aebbf
Tom De Caluwé <decaluwe.t@gmail.com>
2016-09-15 14:14:22 +0200

    Rework the parser tests.

--- test/multiparser.spec.js
+++ test/multiparser.spec.js
@@ -1,84 +1,128 @@
 'use strict'
 
-var expect = require('chai').expect;
+var chai = require('chai');
+var sinon = require('sinon');
+
+chai.use(require('sinon-chai'));
+
+var expect = chai.expect;
 
 var MultiParser = require('../multiparser.js');
 
-describe('MultiParser', function () {
-  var parser;
+describe('The MultiParser caching mechanism', function () {
+  var parser, output;
+  beforeEach(function () {
+    var boundary = 'boundary';
+    output = '';
+    parser = new MultiParser(boundary);
+    parser.on('data', function (data) {
+      output += data.toString();
+    });
+  });
+  it('should contain a newline in it\'s initial state', function () {
+    expect(parser.state).to.equal(MultiParser.states.start);
+    expect(parser.buffer.toString()).to.equal('\r\n');
+    expect(output).to.equal('');
+  });
+  it('should contain a newline after writing an empty string', function () {
+    parser.write('');
+    expect(parser.state).to.equal(MultiParser.states.start);
+    expect(parser.buffer.toString()).to.equal('\r\n');
+    expect(output).to.equal('');
+  });
+  it('should append writes if the maximum cache length isn\'t reached', function () {
+    parser.write(new Buffer('multi'));
+    parser.write(new Buffer('part'));
+    expect(parser.state).to.equal(MultiParser.states.start);
+    expect(parser.buffer.toString()).to.equal('\r\nmultipart');
+    expect(output).to.equal('');
+  });
+  describe('when no boundary is encountered', function () {
+    it('should only keep a buffer with the length of the boundary plus seven', function () {
+      parser.write(new Buffer('some body text before the buffer contents'));
+      expect(parser.state).to.equal(MultiParser.states.body);
+      expect(parser.buffer.toString()).to.equal('buffer contents');
+      expect(output).to.equal('some body text before the ');
+    });
+  });
+  describe('after encountering a boundary', function () {
+    beforeEach(function () {
+      parser.write('Body header\r\n--boundary\r\n');
+    });
+    it('should not process the boundary yet if no more than one additional byte is written', function () {
+      expect(parser.state).to.equal(MultiParser.states.body);
+      expect(parser.buffer.toString()).to.equal('r\r\n--boundary\r\n');
+      expect(output).to.equal('Body heade');
+      parser.write('h');
+      expect(parser.state).to.equal(MultiParser.states.body);
+      expect(parser.buffer.toString()).to.equal('\r\n--boundary\r\nh');
+      expect(output).to.equal('Body header');
+    });
+    it('should process the boundary when at least two additional bytes are written', function () {
+      parser.write('he');
+      expect(parser.state).to.equal(MultiParser.states.headers);
+      // The last line feed is still in the cache because the string search
+      // algorithm didn't confirm yet if it starts a double CRLF sequence or
+      // not. This could be the case if the header was empty.
+      expect(parser.buffer.toString()).to.equal('\nhe');
+      expect(output).to.equal('Body header');
+    });
+  });
+});
+
+describe('The MultiParser boundary processor', function () {
+  var parser, output;
+  var path, current;
+  var parts;
   beforeEach(function () {
     var boundary = 'boundary';
     parser = new MultiParser(boundary);
+    path = [parser];
+    parts = [];
+    var onPart = function (part) {
+      current = part;
+      output = '';
+      current.on('data', function (data) {
+        output += data.toString();
+      });
+      current.on('multi', function () {
+        path.push(current);
+      });
+      current.on('part', function (part) {
+        parts.push(part);
+        onPart(part);
+      });
+      current.on('trailer', function (message) {
+        path.pop();
+        current = message;
+      });
+    }
+    onPart(parser);
   });
-  describe('while parsing a root preamble', function () {
-    it('should keep a lookbehind buffer with the length of the boundary plus seven', function () {
-      parser.write(new Buffer('some body text before the buffer contents'));
-      expect('buffer contents').to.deep.equal(parser.buffer.toString());
+  describe('in the start state', function () {
+    beforeEach(function () {
+      // Check the initial state.
+      expect(parts.length).to.equal(0);
+      expect(parser.margin).to.equal(15);
+      expect(parser.state).to.equal(MultiParser.states.start);
     });
-    it('should append writes if the buffer limit isn\'t reached', function () {
-      parser.write(new Buffer('multi'));
-      parser.write(new Buffer('part'));
-      expect(parser.buffer.slice(-9).toString()).to.deep.equal('multipart');
-  it('should accept non-multipart messages', function () {
-    var event;
-    parser.on('data', function (data) {
-      console.log(data.toString());
+    it('should start a new part when encountering a boundary', function () {
+      // The boundary excluding the trailing newline should be consumed.
+      expect(parser.process(new Buffer('\r\n--boundary\r\nhe'), 0)).to.equal(12);
+      // An additional part should have been produced.
+      expect(parts.length).to.equal(1);
+      expect(parser.margin).to.equal(3);
+      expect(parser.state).to.equal(MultiParser.states.headers);
     });
-    parser.on('finish', function () {
-      event = 'finish';
+    it('should transition to the body state if the data length equals the margin', function () {
+      expect(parser.process(new Buffer('\r\nfourteen chars'), 0)).to.equal(2);
+      expect(output).to.equal('');
+      expect(parser.state).to.equal(MultiParser.states.body);
     });
-    parser.write(new Buffer('header: value\r\n\r\nBody text'));
-    parser.end();
-    expect(event).to.equal('finish');
-  });    });
-  });
-  it('should emit a part event after writing the boundary plus two bytes', function () {
-    var event;
-    parser.on('part', function () { event = 'part'; });
-    parser.write(new Buffer('\r\n--boundary\r\nco'));
-    expect(event).to.equal('part');
-  });
-  it('should start a new multipart message when completing a header with an appropriate content type', function () {
-    var event;
-    parser.on('multi', function () { event = 'multi'; });
-    parser.write(new Buffer('--boundary\r\nContent-Type: multipart/report; boundary=text\r\n\r\nmore body text'));
-    expect(event).to.equal('multi');
-  });
-  it('should be able to apply a new boundary while parsing the same chunk of data', function () {
-    var count = 0;
-    var text = '';
-    text += '--boundary\r\n';
-    text += 'content-type: multipart/mixed; boundary=text\r\n\r\n';
-    text += '--text\r\nco';
-    parser.part = function () {
-      MultiParser.prototype.part.call(this);
-      count++;
-    };
-    parser.write(new Buffer(text));
-    expect(count).to.equal(2);
-  });
-  it('should close the last part when the multipart message is closed', function () {
-    var event;
-    parser.on('part', function (part) {
-      part.on('finish', function () { event = 'finish'; });
+    it('should skip the first two bytes when parsing a preamble', function () {
+      expect(parser.process(new Buffer('\r\npreamble contents'), 0)).to.equal(4);
+      expect(output).to.equal('pr');
+      expect(parser.state).to.equal(MultiParser.states.body);
     });
-    parser.write(new Buffer('\r\n--boundary\r\n\r\n\r\n--boundary--\r\n'));
-    expect(event).to.equal('finish');
-  });
-  it('should close a message part when another boundary is encountered', function () {
-    var event;
-    parser.on('part', function (part) {
-      part.on('finish', function () { event = 'finish'; });
-    });
-    parser.write(new Buffer('\r\n--boundary\r\n\r\n\r\n--boundary\r\nheader: value\r\n'));
-    expect(event).to.equal('finish');
-  });
-  it('should parse boundaries without a trailing CRLF as body text', function () {
-    var output = '';
-    parser.on('data', function (chunk) {
-      output += chunk;
-    });
-    parser.write(new Buffer('\r\n--boundary body text'));
-    expect(output.slice(0, 12)).to.equal('\r\n--boundary');
   });
 });
